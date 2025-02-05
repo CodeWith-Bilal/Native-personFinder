3deb964a891e6f5b556bc879029a7756cbe468da
CodeWithMian <mb03086350364@gmail.com>
2025-02-05 18:08:58 +0500

    Add - [useRedux hook] make seperate styles for all components and screen - correct the spellings

--- src/component/reportItem/ReportItem.tsx
+++ src/component/reportItem/ReportItem.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import {View, Text, Image, TouchableOpacity, StyleSheet} from 'react-native';
+import {View, Text, Image, TouchableOpacity} from 'react-native';
 import {sendEmail} from '../../utils/SendEmail';
 import {ReportItemProps} from '../../types/types';
-import { colors } from '../../constants/colors';
+import { styles } from './reportItemStyle';
 
 const ReportItem = ({item}: ReportItemProps) => (
   <View style={styles.reportCard}>
@@ -32,60 +32,4 @@ const ReportItem = ({item}: ReportItemProps) => (
 
 export default ReportItem;
 
-export const styles = StyleSheet.create({
-  reportCard: {
-    flexDirection: 'row',
-    marginBottom: 20,
-    backgroundColor: colors.white,
-    padding: 16,
-  },
-  reportImage: {
-    width: 115,
-    height: 154,
-    borderRadius: 10,
-    marginRight: 16,
-  },
-  reportDetails: {
-    flex: 1,
-  },
-  reportName: {
-    fontFamily:'Familjen Grotesk',
-    fontSize: 16,
-    fontWeight: '400',
-    color: colors.black,
-  },
-  reportReporter: {
-    fontFamily:'Familjen Grotesk',
-    fontSize: 16,
-    marginTop: 4,
-    fontWeight: '400',
-    color: colors.black,
-  },
-  reportLocation: {
-    fontFamily:'Familjen Grotesk',
-    fontSize: 16,
-    marginTop: 4,
-    fontWeight: '400',
-    color: colors.black,
-  },
-  reportDescription: {
-    fontFamily:'Familjen Grotesk',
-    fontSize: 16,
-    marginTop: 4,
-    fontWeight: '400',
-    color: colors.black,
-  },
-  contactButton: {
-    width: 101,
-    marginTop: 17,
-    paddingVertical: 5,
-    paddingHorizontal: 10,
-    backgroundColor: colors.blue,
-    borderRadius: 5,
-  },
-  contactbtnText: {
-    color: colors.white,
-    fontSize: 11,
-    textAlign: 'center',
-  },
-});
+
